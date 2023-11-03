f9ebe579ae207ed110308234b111e360395a9ab4
Tammy <tammylwalls@gmail.com>
2023-11-02 19:32:03 -0500

    In Employer: added list<Job> jobs, annotated @OneToMany and @JoinColumn("employer") ***need to run this to see if that is the correct colum name for the join annotation
    In Job:
    extended AbstractEntity
    
    commented out the Id and Name fields
    
    updated constructor and employer getter and setter to reflect Employer type rather than String
    
    added @ManyToOne(mappedBy="jobs")to employer field

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,14 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
     @NotBlank(message = "Location is required")
     @Size(min = 1, max = 100, message = "Location must be between 1 and 100 characters long")
     private String location;
+
+//TODO for tam:  verify if "employer" is correct for the JoinColumn
+    @OneToMany
+    @JoinColumn(name = "employer")
+    private List jobs = new ArrayList<>();
 
     public String getLocation() {
         return location;
